cdfbbf96c7a6c319f55254cb151009f08c3a6a6a
ariel6162 <arielkitov@gmail.com>
2021-05-23 15:48:20 +0300

    Update main.js

--- Day 12/ex1-js/main.js
+++ Day 12/ex1-js/main.js
@@ -1,21 +1,35 @@
-console.log('hello world!');
-var car = [1, 2, 3];
-car.x = 20;
-car.y = 29;
+// var Calculator = [a,b];
 
-
-function Person(firstName, lastName)
 {
-    this.firstName = firstName;
-    this.lastName = lastName;
+
+
 }
-Person.prototype.log = function(){
-    console.log(this.firstName);
 }
-var p1 = new Person('ariel', 'kitov');
-var p2 = new Person('dani', 'kitov');
+
+
 
 p1.log();
 p2.log();
 
-console.log(typeof(car));
+// console.log('hello world!');
+// var car = [1, 2, 3];
+// car.x = 20;
+// car.y = 29;
+
+
+// function Person(firstName, lastName)
+// {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     console.log("a");
+// }
+// Person.prototype.log = function(){
+//     console.log(this.firstName);
+// }
+// var p1 = new Person('ariel', 'kitov');
+// var p2 = new Person('dani', 'kitov');
+
+// p1.log();
+// p2.log();
+
+// console.log(typeof(car));
